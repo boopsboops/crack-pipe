450930260de4dc838bc14410b298cc9205af71a0
rupert@it031740 <rupertcollins@gmail.com>
2019-08-01 00:33:38 +0100

    dealing with merged seqs

--- dereplicate.sh
+++ dereplicate.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 # qc filter
-FILES="$(ls temp/trimmed/*.fastq.gz | sed --expression='s/\.fastq\.gz//g' | sed --expression="s/temp\/trimmed\///g" | uniq)"
+FILES="$(ls temp/trimmed/*.fastq.gz | sed --expression='s/\.merged\.fastq\.gz//g' | sed --expression='s/temp\/trimmed\///g')"
 # check
 for f in $FILES; do echo "$f"; done
 # now run
 for i in $FILES; do
-    vsearch --fastq_filter temp/trimmed/"$i".fastq.gz --fastq_maxee 2 --fastq_minlen 105 --fastq_maxlen 200 --fastq_maxns 0 --fastaout temp/filtered/"$i".fasta --fasta_width 0 &
+    vsearch --fastq_filter temp/trimmed/"$i".merged.fastq.gz --fastq_maxee 2 --fastq_minlen 105 --fastq_maxlen 200 --fastq_maxns 0 --fastaout temp/filtered/"$i".fasta --fasta_width 0 &
 done; wait
 
 # derep
-FILES="$(ls temp/filtered/*.fasta | sed --expression='s/\.fasta//g' | sed --expression="s/temp\/filtered\///g" | uniq)"
+FILES="$(ls temp/filtered/*.fasta | sed --expression='s/\.fasta//g' | sed --expression='s/temp\/filtered\///g')"
 # check
 for f in $FILES; do echo "$f"; done
 # now run
