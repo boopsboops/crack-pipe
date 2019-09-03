af0f956fa20a1a62feae3804ab36a1855e1d55c6
rupert@it031740 <rupertcollins@gmail.com>
2019-09-03 12:26:50 +0100

    reduced error value

--- assign-taxonomy.sh
+++ assign-taxonomy.sh
@@ -16,7 +16,7 @@ done
 MINLEN=$(awk "function ceil(x, y){y=int(x); return(x>y?y+1:y)} BEGIN { pc=${AVG}*${PROP}; i=ceil(pc); print i }")
 
 # trim primers from the custom reference library
-cutadapt -n 1 -e 0.3 -O 10 -g GTCGGTAAAACTCGTGCCAGC temp/reference-library/custom-references-annotated.fasta | cutadapt --minimum-length "$MINLEN" -n 1 -e 0.3 -O 10 -a CAAACTGGGATTAGATACCCCACTATG -o temp/reference-library/custom-references-annotated.trimmed.fasta -
+cutadapt -n 1 -e 0.2 -O 10 -g GTCGGTAAAACTCGTGCCAGC temp/reference-library/custom-references-annotated.fasta | cutadapt --minimum-length "$MINLEN" -n 1 -e 0.2 -O 10 -a CAAACTGGGATTAGATACCCCACTATG -o temp/reference-library/custom-references-annotated.trimmed.fasta -
 
 # merge with refseq
 cat temp/reference-library/custom-references-annotated.trimmed.fasta assets/refseq-mtdna-with-taxonomy.fasta > results/reference-library.fasta
