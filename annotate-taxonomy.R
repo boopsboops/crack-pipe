46791d2a63fc66308e624a4d0547c8c524fb9f30
rupert@it031740 <rupertcollins@gmail.com>
2019-12-16 20:59:32 +0000

    removed ncbi taxize query

--- annotate-taxonomy.R
+++ annotate-taxonomy.R
@@ -4,8 +4,6 @@ suppressMessages(library("ape"))
 suppressMessages(library("tidyverse"))
 suppressMessages(library("parallel"))
 suppressMessages(library("magrittr"))
-suppressMessages(library("taxize"))
-data(fishbase)
 
 # read in refs
 custom.refs <- read.FASTA(file="temp/reference-library/custom-references.fasta")
@@ -14,53 +12,53 @@ custom.refs <- read.FASTA(file="temp/reference-library/custom-references.fasta")
 custom.df <- tibble(code=str_split_fixed(names(custom.refs),"\\|",2)[,1],sciName=str_split_fixed(names(custom.refs),"\\|",2)[,2]) %>%
     mutate(Genus=str_split_fixed(sciName," ",2)[,1])
 
-# get up to date spp list
-fishbase.tbl <- rfishbase::species()
-# make genus
-fishbase.tbl %<>% mutate(genus=str_split_fixed(Species," ",2)[,1])
+# get up to date spp list and taxonomy
+fishbase.species <- rfishbase::species(server="fishbase")
+fishbase.taxonomy <- rfishbase::load_taxa(server="fishbase")
+fishbase.synonyms <- rfishbase::synonyms(server="fishbase")
 
-# add families code
-custom.df %<>% mutate(famCode=pull(fishbase.tbl,FamCode)[match(Genus,pull(fishbase.tbl,genus))])
+# anotate the dataframe with taxonomy
+custom.df.annotated <- custom.df %>% 
+    mutate(Family=pull(fishbase.taxonomy,Family)[match(Genus,pull(fishbase.taxonomy,Genus))],
+    Order=pull(fishbase.taxonomy,Order)[match(Genus,pull(fishbase.taxonomy,Genus))],
+    Class=pull(fishbase.taxonomy,Class)[match(Genus,pull(fishbase.taxonomy,Genus))])
 
-# check which are complete
-not.df <- custom.df %>% filter(is.na(famCode))
+# get the genera not annotated
+annotated.not <- custom.df.annotated %>% filter(is.na(Family)) %>% pull(Genus) %>% unique()
 
-# consult NCBI for families of those missing
-not.ncbi <- taxize::classification(taxize::genbank2uid(id=pull(not.df,code)),db="ncbi",messages=FALSE)
+# get the taxonomic info for genus synonyms
+fishbase.synonyms.not <- fishbase.synonyms %>% 
+    mutate(Genus=str_split_fixed(synonym," ",2)[,1]) %>% 
+    filter(Genus %in% annotated.not) %>% 
+    mutate(Family=pull(fishbase.taxonomy,Family)[match(SpecCode,pull(fishbase.taxonomy,SpecCode))],
+    Order=pull(fishbase.taxonomy,Order)[match(SpecCode,pull(fishbase.taxonomy,SpecCode))],
+    Class=pull(fishbase.taxonomy,Class)[match(SpecCode,pull(fishbase.taxonomy,SpecCode))])
 
-# extract family from result and add to df
-extract_fam <- function(x){x %>% filter(rank=="family") %>% pull(name)}
-not.df %<>% mutate(fam=purrr::map_chr(not.ncbi,extract_fam))
-
-# get fam codes 
-not.df %<>% mutate(famCode=pull(fishbase,FamCode)[match(fam,pull(fishbase,Family))])
+# add this info to the df
+custom.df.annotated %<>% mutate(Family=if_else(is.na(Family),pull(fishbase.synonyms.not,Family)[match(Genus,pull(fishbase.synonyms.not,Genus))],Family),
+    Order=if_else(is.na(Order),pull(fishbase.synonyms.not,Order)[match(Genus,pull(fishbase.synonyms.not,Genus))],Order),
+    Class=if_else(is.na(Class),pull(fishbase.synonyms.not,Class)[match(Genus,pull(fishbase.synonyms.not,Genus))],Class))
 
 # check names validity
 print("The following taxa could not be found. Spelling error, or maybe a try a synonym? If empty, all taxa were found.")
-not.df %>% filter(is.na(famCode)) %>% print(n=Inf)
+custom.df.annotated %>% filter(is.na(Family)) %>% print(n=Inf)
 
 # filter the NAs remove temp fam
-not.df %<>% filter(!is.na(famCode)) %>% select(-fam)
-
-# merge
-joined.df <- bind_rows(custom.df,not.df) %>% filter(!is.na(famCode))
+custom.df.annotated %<>% filter(!is.na(Family))
 
 # annotate with fb
-joined.df %<>% 
-    mutate(family=pull(fishbase,Family)[match(famCode,pull(fishbase,FamCode))],
-    order=pull(fishbase,Order)[match(famCode,pull(fishbase,FamCode))],
-    class=pull(fishbase,Class)[match(famCode,pull(fishbase,FamCode))],
-    kingdom="Animalia",phylum="Chordata") %>%
+custom.df.annotated %<>% 
+    mutate(Kingdom="Animalia",Phylum="Chordata") %>%
     mutate(sciName=str_replace_all(sciName,":",".")) %>%
-    mutate(label=paste0(code,";tax=k:",kingdom,",p:",phylum,",c:",class,",o:",order,",f:",family,",g:",Genus,",s:",sciName)) %>% 
+    mutate(label=paste0(code,";tax=k:",Kingdom,",p:",Phylum,",c:",Class,",o:",Order,",f:",Family,",g:",Genus,",s:",sciName)) %>% 
     mutate(label=str_replace_all(label," ","_"))
 
 # trim names and remove non-matched
 names(custom.refs) <- str_split_fixed(names(custom.refs),"\\|",2)[,1]
-custom.refs.sub <- custom.refs[which(names(custom.refs) %in% pull(joined.df,code))]
+custom.refs.sub <- custom.refs[which(names(custom.refs) %in% pull(custom.df.annotated,code))]
 
 # add new names
-names(custom.refs.sub) <- pull(joined.df,label)[match(names(custom.refs.sub), pull(joined.df,code))]
+names(custom.refs.sub) <- pull(custom.df.annotated,label)[match(names(custom.refs.sub), pull(custom.df.annotated,code))]
 
 # write out
 write.FASTA(custom.refs.sub,file="temp/reference-library/custom-references-annotated.fasta")
