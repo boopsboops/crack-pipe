481423ea7ef00ed9d591ff8685262ae2125afc4b
rupert@sorubim <rupertcollins@gmail.com>
2019-12-21 23:47:20 +0000

    added new dirs

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # make temp folders
-mkdir -p results temp/fastq temp/merged temp/clustered temp/reorientated temp/dereplicated temp/demultiplexed temp/filtered temp/trimmed temp/trash temp/reference-library temp/blast-dump
+mkdir -p results temp/user-data temp/merged temp/clustered temp/reorientated temp/dereplicated temp/demultiplexed temp/filtered temp/trimmed temp/trash temp/reference-library temp/blast-dump temp/samples
